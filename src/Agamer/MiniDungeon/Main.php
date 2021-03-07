70b8c3b42d87a7e126f285dfb4cf017240fbd572
AgaricaGamer <69802631+AgaricaGamer@users.noreply.github.com>
2021-03-07 15:06:39 +0530

    Update Main.php

--- src/Agamer/MiniDungeon/Main.php
+++ src/Agamer/MiniDungeon/Main.php
@@ -33,5 +33,97 @@ class Main extends PluginBase implements Listener{
                 $player->addXp($event->getXpDropAmount());
                 $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
                 $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("delay"));
-              elseif($block->getId() === Block::IRON_ORE && $this->config->get("iron_ore") === true){
+            } elseif($block->getId() === Block::IRON_ORE && $this->config->get("iron_ore") === true){
+}
+                $event->setDrops([]);
+                $event->setCancelled();
+                $event->setXpDropAmount(0);
+                $player->addXp($event->getXpDropAmount());
+                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("delay"));
+            } elseif($block->getId() === Block::GOLD_ORE && $this->config->get("gold_ore") === true){
+                foreach ($event->getDrops() as $drop) {
+                    $event->getPlayer()->getInventory()->addItem($drop);
+ }
+                $event->setDrops([]);
+                $event->setCancelled();
+                $event->setXpDropAmount(0);
+                $player->addXp($event->getXpDropAmount());
+                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("delay"));
+            } elseif($block->getId() === Block::DIAMOND_ORE && $this->config->get("diamond_ore") === true){
+                foreach ($event->getDrops() as $drop) {
+                    $event->getPlayer()->getInventory()->addItem($drop);
+                }
+                $event->setDrops([]);
+                $player->addXp($event->getXpDropAmount());
+                $event->setCancelled();
+                $event->setXpDropAmount(0);
+                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("delay"));
+            } elseif($block->getId() === Block::EMERALD_ORE && $this->config->get("emerald_ore") === true){
+                foreach ($event->getDrops() as $drop) {
+                    $event->getPlayer()->getInventory()->addItem($drop);
+}
+                $event->setDrops([]);
+                $event->setCancelled();
+                $event->setXpDropAmount(0);
+                $player->addXp($event->getXpDropAmount());
+                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("delay"));
+            } elseif($block->getId() === Block::DIAMOND_ORE && $this->config->get("diamond_ore") === true){
+                foreach ($event->getDrops() as $drop) {
+                    $event->getPlayer()->getInventory()->addItem($drop);
+                }
+                $event->setDrops([]);
+                $player->addXp($event->getXpDropAmount());
+                $event->setCancelled();
+                $event->setXpDropAmount(0);
+                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("delay"));
+            } elseif($block->getId() === Block::EMERALD_ORE && $this->config->get("emerald_ore") === true){
+                foreach ($event->getDrops() as $drop) {
+                    $event->getPlayer()->getInventory()->addItem($drop);
+                }
+                $event->setDrops([]);
+                $player->addXp($event->getXpDropAmount());
+                $event->setCancelled();
+                $event->setXpDropAmount(0);
+                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("delay"));
+            } elseif($block->getId() === Block::QUARTZ_ORE && $this->config->get("quartz_ore") === true){
+                foreach ($event->getDrops() as $drop) {
+                    $event->getPlayer()->getInventory()->addItem($drop);
+                }
+                $event->setDrops([]);
+                $player->addXp($event->getXpDropAmount());
+                $event->setCancelled();
+                $event->setXpDropAmount(0);
+                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("delay"));
+            } elseif($block->getId() === Block::LAPIS_ORE && $this->config->get("lapis_ore") == true){
+                foreach ($event->getDrops() as $drop) {
+                    $event->getPlayer()->getInventory()->addItem($drop);
+                }
+                $event->setDrops([]);
+                $player->addXp($event->getXpDropAmount());
+                $event->setCancelled();
+                $event->setXpDropAmount(0);
+                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("delay"));
+            } elseif($block->getId() === Block::REDSTONE_ORE && $this->config->get("redstone_ore") === true){
+                foreach ($event->getDrops() as $drop) {
+                    $event->getPlayer()->getInventory()->addItem($drop);
+                }
+                $event->setDrops([]);
+                $player->addXp($event->getXpDropAmount());
+                $event->setCancelled();
+                $event->setXpDropAmount(0);
+                $block->getLevelNonNull()->setBlock($block->asVector3(), Block::get(Block::BEDROCK));
+                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $block), 20 * $this->config->get("delay"));
+            }
+        }
+    }
+
+}
       
